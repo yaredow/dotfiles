9daa420f0eb8297a704cd564c25d173fac83f826
yaredow <yaredyilma11@gmail.com>
2026-05-06 19:36:51 +0000

    feat(nvim): configure lsp for typescript, golang, and python

--- nvim/.config/nvim/lua/yada/lazy.lua
+++ nvim/.config/nvim/lua/yada/lazy.lua
@@ -1,17 +1,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable", 
     lazypath,
   })
 end
-
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("yada.plugins", {
+
+require("lazy").setup({ { import = "yada.plugins" }, { import = "yada.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
@@ -20,4 +20,3 @@ require("lazy").setup("yada.plugins", {
     notify = false,
   },
 })
-
