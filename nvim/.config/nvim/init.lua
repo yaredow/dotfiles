3b9d171a1e7d73d2a9233e0d7aa3061b5f0dd7a7
yaredow <yaredyilma11@gmail.com>
2026-05-13 21:49:06 +0300

    fix(nvim-blink): configure enter for accepting completion

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -852,6 +852,7 @@ do
       ['<C-j>'] = { 'select_next', 'fallback' },
       ['<C-k>'] = { 'select_prev', 'fallback' },
       ['<C-l>'] = { 'show_signature', 'fallback' },
+      ['<CR>'] = { 'accept', 'fallback' },
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
