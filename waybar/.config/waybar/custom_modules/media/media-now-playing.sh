ac6b90ace389a5e38e58f36bd84d81a5012a636e
yardow <yaredyilma11@gmail.com>
2026-05-17 11:11:47 +0000

    feat(waybar): create the perfect waybar with sliding current playing media indicator

---
+++ waybar/.config/waybar/custom_modules/media/media-now-playing.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+zscroll -l 20 \
+    --delay 0.3 \
+    --update-check true \
+    "playerctl metadata --format '{{title}} - {{artist}}'" 2>/dev/null &
+
+# Keep script running
+wait
