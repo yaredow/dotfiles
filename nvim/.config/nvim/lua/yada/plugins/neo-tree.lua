c6e0e7fddabf7f86d1c4cb677581b4faf5ea2eab
yardow <yaredyilma11@gmail.com>
2026-06-26 20:28:33 +0300

    feat(kitty): configure kitty to render font nicely

--- nvim/.config/nvim/lua/yada/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/yada/plugins/neo-tree.lua
@@ -10,6 +10,22 @@ return {
     window = {
       position = 'right',
     },
+    event_handlers = {
+      {
+        event = 'before_render',
+        handler = function(state)
+          if not state._short_root_name then
+            local original_name = state.components.name
+            state.components.name = function(config, node, s)
+              local result = original_name(config, node, s)
+              if result and result.text and node:get_depth() == 1 then result.text = vim.fn.fnamemodify(result.text, ':t') end
+              return result
+            end
+            state._short_root_name = true
+          end
+        end,
+      },
+    },
     filesystem = {
       hijack_netrw_behavior = 'open_default',
       follow_current_file = {
